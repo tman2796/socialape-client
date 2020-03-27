e1687b95609f5a9a304641dd0f00d5a1245ca38d
Travis Simmons <tsimmons6@ggc.edu>
2020-03-27 17:52:56 -0400

    Created login form and verifies credentials and provides necessary errors

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
-import Link from 'react-router-dom/Link';
+import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime'
 // // Mui
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -15,10 +17,10 @@ const styles = {
         display: 'flex',
         marginBottom: 20
     },
-    image:{
+    image: {
         minWidth: 200
     },
-    content:{
+    content: {
         padding: 25,
         objectFit: 'cover'
     }
@@ -26,6 +28,7 @@ const styles = {
 
 class Scream extends Component {
     render() {
+        dayjs.extend(relativeTime)
         const {
             classes,
             scream: {
@@ -45,14 +48,17 @@ class Scream extends Component {
 
         return (
             <Card className={classes.card}>
-                <CardMedia image={userImage} title="Profile image" className={classes.image}/>
-                <CardContent class={classes.content}>
+                <CardMedia
+                    image={userImage}
+                    title="Profile image" className={classes.image}
+                />
+                <CardContent className={classes.content}>
                     <Typography variant="h5" component={Link} to={`/users/${userHandle}`}
                         color="primary">
                         {userHandle}
                     </Typography>
                     <Typography variant="body2" color="textSecondary">
-                        {createdAt}
+                        {dayjs(createdAt).fromNow()}
                     </Typography>
                     <Typography variant="body1">
                         {body}
--- src/pages/home.js
+++ src/pages/home.js
@@ -21,7 +21,7 @@ class home extends Component {
     }
     render() {
         let recentScreamsMarkup = this.state.screams ? (
-            this.state.screams.map((scream) => <Scream scream={scream} />)
+            this.state.screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
         ) : (<p>Loading...</p>);
         return (
             <Grid container spacing={10}>
